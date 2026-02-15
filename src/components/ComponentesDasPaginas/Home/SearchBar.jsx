56b0da6116741d986ded99fec3a3e11a9d76e734
walberluis <wlsp@ic.ufal.br>
2026-02-15 18:56:10 -0300

    :bug: fix: consertando interface

--- src/components/ComponentesDasPaginas/Home/SearchBar.jsx
+++ src/components/ComponentesDasPaginas/Home/SearchBar.jsx
@@ -4,13 +4,13 @@ import { Search } from 'lucide-react';
 const SearchBar = ({ searchTerm, onSearchChange }) => {
   return (
     <div className="relative">
-      <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+      <Search className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400 bg-white" />
       <input
         type="text"
         placeholder="Buscar aluno"
         value={searchTerm}
         onChange={(e) => onSearchChange(e.target.value)}
-        className="pl-12 pr-6 py-3 rounded-full border border-blue focus:outline-none focus:ring-2 focus:ring-blue-500 w-80"
+        className="pl-12 pr-6 py-3 rounded-full border border-yellow focus:none focus:ring-2 focus:ring-blue-500 w-80"
       />
     </div>
   );
