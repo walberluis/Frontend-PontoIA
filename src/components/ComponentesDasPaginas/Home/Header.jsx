aefc91e2ced17eb4938c2d0a15dbc0d00f88310e
walberluis <wlsp@ic.ufal.br>
2026-03-09 01:17:43 -0300

    :bug: fix: alterando diretório da logo

--- src/components/ComponentesDasPaginas/Home/Header.jsx
+++ src/components/ComponentesDasPaginas/Home/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../../assets/logo-academy.png'
 
 const Header = () => {
   return (
     <div className="flex items-center justify-between mb-16">
       <h1 className="text-5xl font-bold text-[#4493AC]">Home</h1>
       <img
-        src="/src/assets/logo-academy.png"
+        src={logo}
         alt="Logo"
         className="h-12 w-auto object-contain"
       />
